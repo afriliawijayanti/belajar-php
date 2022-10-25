cb205cf9cbc2bf7e6faee28a36632347165f8593
Afrilia Wijayanti <afriliawijayanti20@gmail.com>
2022-10-25 20:29:43 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+    echo "Hallo word!<br>";
+
+    $nama = "Afrilia Wijayanti";
+    $umur = 21;
+
+    echo "Nama saya <strong>$nama</strong>, saya berusia $umur tahun.<br>";
+
+    $namaAdik = "Ferryan Prima Sanjaya";
+    $umurAdik = 13;
+
+    echo "Nama adik saya <strong>$namaAdik</strong>, adik saya berusia $umurAdik tahun.<br>";
+
+    $selisihUmur = $umur-$umurAdik;
+    echo "Selisih usia saya dan adik saya adalah $selisihUmur tahun."
+
+
+?>
